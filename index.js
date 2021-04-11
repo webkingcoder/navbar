bad5f4a6363c8c52de2b6fde2b13daf51bae1cef
webkingcoder <71870509+webkingcoder@users.noreply.github.com>
2021-04-11 23:13:54 +0530

    a

---
+++ index.js
@@ -0,0 +1,8 @@
+// window.onload()
+    // let preloader = select('#preloader');
+    // if (preloader) {
+        // window.addEventListener('load', () => {
+            // preloader.remove()
+        // },1500);
+    // }
+// 
